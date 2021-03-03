cbab622dfcc78b36f325414d27dec4eb7042da31
CatherineLuje <katyluje@gmail.com>
2021-03-03 16:15:43 -0500

    GeoSpace

--- Geo space/src/geo/space/GeoSpace.java
+++ Geo space/src/geo/space/GeoSpace.java
@@ -22,10 +22,10 @@ public class GeoSpace {
         int option;
 
         do {
-  option = printConversionMenu(input);
+            option = printConversionMenu(input);
 
-            switch (option) {                 
-                    case 1:
+            switch (option) {
+                case 1:
                     int totalOf = 0;
                     System.out.print(" Enter the number of times you want to perform menu 1 -> ");
                     totalOf = input.nextInt();
@@ -41,7 +41,7 @@ public class GeoSpace {
 
                     transformRectangularToGeographicCoordinates(totalOf, input, x, y, angleF);
 
-                    break;    
+                    break;
                 case 2:
 
                     double d;
@@ -109,6 +109,32 @@ public class GeoSpace {
                             System.out.println("The coordinate is:  X=" + String.format("%.2f", x5) + ", Y=-" + String.format("%.2f", y5));
                             break;
                     }
+                case 3:
+                    int total = 0;
+                    System.out.print(" Enter the number of times you want to perform menu 1 -> ");
+                    total = input.nextInt();
+                    for (int i = 0; i < total; i++) {
+                        double dis;
+                        double[] x1;
+                        double[] y1;
+                        double[] x2;
+                        double[] y2;
+                        double variationX;
+                        double variationY;
+                        double azimut = 0;
+                        x1 = new double[total];
+
+                        x1 = new double[total];
+                        y1 = new double[total];
+                        x2 = new double[total];
+                        y2 = new double[total];
+
+                        azimut = transformGeographicCoordinatesToPolar(x1, i, input, y1, x2, y2);
+
+                        // Transformation from decimals to sexagecimals
+                        printDecimalToSexagecimalTransformation(azimut);
+                    }
+                    break;
                 case 0:
                     System.out.println("Good Bye my friend");
                     System.exit(0);
@@ -120,8 +146,8 @@ public class GeoSpace {
 
         } while (option != 0);
     }
- 
- private static int printConversionMenu(Scanner input) {            
+
+    private static int printConversionMenu(Scanner input) {
         int option;
         System.out.print("                   UNIVERSIDAD DE LAS FUERZAS ARMADAS (ESPE)\n\n       ");
         System.out.print(" CAREER: Engineering in Geospatial Technologies\n ");
@@ -146,7 +172,7 @@ public class GeoSpace {
             y[j] = input.nextInt();
         }
         return 0;
-    }        
+    }
 
     private static void transformRectangularToGeographicCoordinates(int totalOf, Scanner input, double[] x, double[] y, double angleF) {
 
@@ -178,7 +204,54 @@ public class GeoSpace {
             }
         }
     }
-}
 
-    
-          
+    public static double transformGeographicCoordinatesToPolar(double[] x1, int i, Scanner input, double[] y1, double[] x2, double[] y2) {
+        double variationX;
+        double variationY;
+        double distance;
+        double azimut;
+        System.out.println("--Coordinate A--");
+        System.out.print("Enter rectangular coordinate x: ");
+        x1[i] = input.nextDouble();
+        System.out.print("Enter rectangular coordinate y: ");
+        y1[i] = input.nextDouble();
+        System.out.println("--Coordinate B--");
+        System.out.print("Enter rectangular coordinate x: ");
+        x2[i] = input.nextDouble();
+        System.out.print("Enter rectangular coordinate y: ");
+        y2[i] = input.nextDouble();
+        variationX = x2[i] - x1[i];
+        variationY = y2[i] - y1[i];
+        distance = Math.sqrt(Math.pow(variationX, 2) + Math.pow(variationY, 2));
+        azimut = Math.atan(variationX / variationY);
+        azimut = azimut + 360;
+        if (variationX > 0 & variationY > 0) {
+            System.out.println("The azimuth is: " + String.format("%.2f", azimut));
+        }
+        if (variationX > 0 & variationY < 0) {
+            System.out.println("The azimuth is: " + String.format("%.2f", azimut));
+        }
+        if (variationX < 0 & variationY > 0) {
+            azimut = azimut - 180;
+            System.out.println("The azimuth is: " + String.format("%.2f", azimut));
+        }
+        if (variationX < 0 & variationY < 0) {
+            azimut = azimut + 180;
+            System.out.println("The azimuth is: " + String.format("%.2f", azimut));
+        }
+        return azimut;
+
+    }
+
+    public static void printDecimalToSexagecimalTransformation(double azimut) {
+        int degrees = (int) azimut;
+        double fractionalDegrees = azimut - degrees;
+        double minutesWithFraction = 60 * fractionalDegrees;
+        int minutes = (int) minutesWithFraction;
+        double fractionalMinutes = minutesWithFraction - minutes;
+        double secondsWithFraction = 60 * fractionalMinutes;
+        int seconds = (int) Math.round(secondsWithFraction);
+        System.out.println("The polar coordinate is: " + degrees + "°" + minutes + "'" + seconds + "'' ");
+
+    }
+}
