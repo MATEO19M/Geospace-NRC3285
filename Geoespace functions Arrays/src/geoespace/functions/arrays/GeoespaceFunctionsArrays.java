f53bd48bc3a83266fb7d0c5503bb3f7d08cfceb5
Erick0112 <erickespe2001@hotmail.com>
2021-03-05 12:14:18 -0500

    Geo space

---
+++ Geoespace functions Arrays/src/geoespace/functions/arrays/GeoespaceFunctionsArrays.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geoespace.functions.arrays;
+
+/**
+ *
+ * @author Pc
+ */
+public class GeoespaceFunctionsArrays {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
